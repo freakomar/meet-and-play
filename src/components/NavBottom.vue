e8891f63203d0632346dec38083dc9ffa333f7c9
FreeZZZeee <amazing.leo@yandex.ru>
2021-03-25 22:04:13 +0500

    Создал компоненты

---
+++ src/components/NavBottom.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="bottom-list-link">
+    <li class="link">
+      <router-link to="/settings" class="bottom-link" active-class="active-link"
+        >Настройки</router-link
+      >
+    </li>
+    <li class="link">
+      <router-link to="/support" class="bottom-link" active-class="active-link"
+        >Помощь</router-link
+      >
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "MapNavBottom",
+  component: {},
+};
+</script>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="top-list-link">
+    <li class="link">
+      <router-link to="/" class="top-link" active-class="active-link"
+        >Учавствую</router-link
+      >
+    </li>
+    <li class="link">
+      <router-link to="/myMeetings" class="top-link" active-class="active-link"
+        >Мои встречи</router-link
+      >
+    </li>
+    <li class="link">
+      <router-link to="/create" class="top-link" active-class="active-link"
+        >Создать встречу</router-link
+      >
+    </li>
+    <li class="link">
+      <router-link to="/favorites" class="top-link" active-class="active-link"
+        >Избранное</router-link
+      >
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "NavItem",
+};
+</script>
