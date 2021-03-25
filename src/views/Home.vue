82b4f44811480e15a32c3960e5504d606ba988ae
Alexander <alx925@gmail.com>
2021-03-25 15:26:06 +0300

    Подключение Layout для приложения и ройтинг /login /registration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="fff">
+    <h2>Здесь будет отоброжаться основной контент разных страниц</h2>
+    <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
   </div>
 </template>
 
